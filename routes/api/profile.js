108aa0e202b65a1784687ad3b8042bda95e0df46
Patrick Loughrey <ploughrey93@gmail.com>
2019-09-25 23:57:45 -0400

    Successfully authenticated with Mongo cluster

---
+++ routes/api/profile.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// @route           GET api/profile
+// @description     Register user
+// @access          Public
+router.get("/", (req, res) => {
+  res.send("Profile route");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect Database
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("App is up and running!");
 });
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 app.listen(PORT, () => {
   console.log(`Server up and running on port ${PORT}`);
